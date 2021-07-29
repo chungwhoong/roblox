555e7eb7f9a7ce1f8c9db0b817b9e34c1b849d03
chungwhoong <46268649+chungwhoong@users.noreply.github.com>
2021-07-30 08:08:45 +0900

    Create StopFireScript.lua

---
+++ StopFireScript.lua
@@ -0,0 +1,29 @@
+local box=script.parent
+local point=10
+
+
+local Players=game:GetService("Players")
+
+local function stopFire(player)
+	
+	for fireSize=12,0,-1 do
+		box.Fire.size=fireSize
+	end
+	
+	wait(2)
+	box:Destroy()
+end
+
+local function partTouched(otherPart)
+	local player=game.Players:GetPlayerFromCharacter(otherPart.Parent)
+	
+	if player then
+		stopFire()
+		local playerStats=player:WaitForChild('leaderstats') --플레이어의 점수판을 playerStats 변수로 지정
+		local playerPoint=playerStats:WaitForChild('Point') --플레이어의 포인트를 playerPoint 변수로 지정
+
+		playerPoint.Value=playerPoint.Value+point
+	end
+end
+
+box.Touched:Connect(partTouched)
