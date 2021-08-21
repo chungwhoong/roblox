82f65c9c935a12ffd7eaaf64cbdd908c0ceaeef5
chungwhoong <46268649+chungwhoong@users.noreply.github.com>
2021-08-22 07:52:16 +0900

    Update BuyCar.lua

--- BuyCar.lua
+++ BuyCar.lua
@@ -2,6 +2,8 @@ local platform=script.parent
 local blackCarWall=game.Workspace.BlackCarWall
 local Players=game:GetService("Players")
 
+local carPrice=30
+
 local wall1=blackCarWall.Wall1
 local wall2=blackCarWall.Wall2
 local wall3=blackCarWall.Wall3
@@ -17,17 +19,22 @@ local function destroyWall()
 	end
 end
 
+local function buyCar(player)
+	local playerStats=player:WaitForChild('leaderstats') --플레이어의 점수판을 playerStats 변수로 지정
+	local playerPoint=playerStats:WaitForChild('Point') --플레이어의 포인트를 playerPoint 변수로 지정
+	
+	if playerPoint.Value>=0 then
+		playerPoint.Value=playerPoint.Value-carPrice
+		destroyWall()
+	end
+
+end
+
 local function partTouched(otherPart)
-	local player=game.Players:GetPlayerFromCharacter(otherPart.Parent)
+	local player=game.Players:GetPlayerFromCharacter(otherPart.Parent) --접촉한 부위로부터 해당 플레이어를 찾아내어 player 변수로 지정
 
-	if player then
-		local playerStats=player:WaitForChild('leaderstats') 
-		local point=playerStats:WaitForChild('Point')		
-
-		if point.value>=0 then
-			destroyWall()
-			point.value=point.value-30
-		end
+	if player then   --만약 플레이어라면
+		buyCar(player)  --givePoint 함수 실행
 	end
 end
 
