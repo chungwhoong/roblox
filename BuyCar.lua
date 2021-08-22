d5e91dccde77160dd98c5cde87134aa8e7b17c32
chungwhoong <46268649+chungwhoong@users.noreply.github.com>
2021-08-23 06:08:47 +0900

    Update BuyCar.lua

--- BuyCar.lua
+++ BuyCar.lua
@@ -23,7 +23,7 @@ local function buyCar(player)
 	local playerStats=player:WaitForChild('leaderstats') --플레이어의 점수판을 playerStats 변수로 지정
 	local playerPoint=playerStats:WaitForChild('Point') --플레이어의 포인트를 playerPoint 변수로 지정
 	
-	if playerPoint.Value>=0 then
+	if playerPoint.Value>=carPrice then
 		playerPoint.Value=playerPoint.Value-carPrice
 		destroyWall()
 	end
