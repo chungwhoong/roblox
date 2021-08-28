bf348c29f92a9ec18fb66748a98012edc15f81d8
chungwhoong <46268649+chungwhoong@users.noreply.github.com>
2021-08-29 00:29:07 +0900

    Update StreetLightScript.lua

--- StreetLightScript.lua
+++ StreetLightScript.lua
@@ -1,20 +1,20 @@
 local time=game.Lighting
 local timeVal=0
 
-local streetLight=script.parent
+local Light=script.parent
 
-local spotLight=streetLight.Light.SpotLight
+local spotLight=Light.SpotLight
 
 while true do
 	time.ClockTime=timeVal
 	print(timeVal)
 	wait(1)
 	timeVal=timeVal+1
-	
+
 	if timeVal==25 then
 		timeVal=0
 	end
-	
+
 	if timeVal>17 or timeVal<6 then
 		spotLight.Enabled=true
 	else
