b08500f2ceed6475d1d912510a6d85c1e92f339a
chungwhoong <46268649+chungwhoong@users.noreply.github.com>
2021-12-31 15:40:55 +0900

    Update FireDamage.lua

--- FireDamage.lua
+++ FireDamage.lua
@@ -1,11 +1,11 @@
-trash=script.Parent
+trashTop=script.Parent
 
 local function fireDamage(otherPart)
 	local partParent=otherPart.Parent  
 	local humanoid=partParent:FindFirstChildWhichIsA('Humanoid') 
-	if humanoid and trash.Fire.Enabled==true then  
+	if humanoid and trashTop.Fire.Enabled==true then  
 		humanoid.Health=humanoid.Health-5
 	end	
 end
 
-trash.Touched:Connect(fireDamage)
+trashTop.Touched:Connect(fireDamage)
